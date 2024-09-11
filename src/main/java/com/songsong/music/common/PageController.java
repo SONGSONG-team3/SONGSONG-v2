56b747667f64c739236f12ae7f8eeb726c50caea
ssIIIn <gomee312@naver.com>
2024-09-11 14:54:50 +0900

    feat: 메인페이지 UI 구현

---
+++ src/main/java/com/songsong/music/common/PageController.java
@@ -0,0 +1,31 @@
+package com.songsong.music.common;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    // 메인 페이지 이동
+    @GetMapping("/") // /pages/main -> /
+    public String main() {
+        return "main";  // main.jsp로 이동
+    }
+
+    // 회원가입 페이지 이동
+    @GetMapping("/pages/user")
+    public String register() {
+        return "register";  // register.jsp로 이동
+    }
+
+    // 로그인 페이지 이동
+    @GetMapping("/pages/login")
+    public String login() {
+        return "login";  // login.jsp로 이동
+    }
+
+    // 게시판 페이지 이동
+    @GetMapping("/pages/board")
+    public String board() {
+        return "board";  // board.jsp로 이동
+    }
+}
